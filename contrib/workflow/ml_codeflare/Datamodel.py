f5495e707843353aa4c00ed63e4332ba5ba1daf6
Yuan-Chi Chang <yuanchi@us.ibm.com>
2021-09-27 09:46:32 -0400

    added helper functions to Pipeline class

--- contrib/workflow/ml_codeflare/Datamodel.py
+++ contrib/workflow/ml_codeflare/Datamodel.py
@@ -18,11 +18,21 @@ class ExecutionType(Enum):
     FIT = 0,
     PREDICT = 1,
     SCORE = 2
+    
+def simplenode(inputtuple, handler):
+    assert handler is not None
+    handler = workflow.get_actor(handler)
+    outputtuple = handler.run_workflow_step.run_async(inputtuple)
+    return outputtuple
 
 @ray.workflow.virtual_actor
 class PersistedDAG():
     def __init__(self, dag):
         self.__dag = dag
+    def set_dag(self, dag):
+        self.__dag = dag
+    def get_dag(self):
+        return self.__dag
     def __getstate__(self):
         return self.__dag
     def __setstate__(self, dag):
@@ -82,16 +92,23 @@ class MLNode():
     def __setstate__(self, estimator):
         self.estimator = estimator
 
-def simplenode(inputtuple, handler):
-    assert handler is not None
-    handler = workflow.get_actor(handler)
-    outputtuple = handler.run_workflow_step.run_async(inputtuple)
-    return outputtuple
-
 class EstimatorNode():
     def __init__(self, node_name: str, estimator: BaseEstimator):
         self.__virtual_actor = MLNode.get_or_create(node_name, estimator)
         self.__node_name = node_name
+    def get_node_name(self):
+        """
+        Returns the node name
+        :return: The name of this node
+        """
+        return self.__node_name
+    def get_estimator(self):
+        """
+        Return the estimator of the node
+        :return: The node's estimator
+        """
+        node_actor = workflow.get_actor(self.__node_name)
+        return node_actor.get_model.run_async()
     def get_node(self):
         func_dict = {}
         func_string = '@contrib.workflow.node\ndef xxxx(inputtuple):\n\treturn simplenode(inputtuple, "yyyy")\nfunc_dict["yyyy"]=xxxx\n'
@@ -107,6 +124,7 @@ class Pipeline:
         self.__id = None
         if id is not None:
             self.__id = id
+            self.__persisteddag = PersistedDAG.get_or_create(self.__id, self.__dag)
     def __str__(self):
         return self.__id__
     def add_edge(self, from_node, to_node):
@@ -115,14 +133,20 @@ class Pipeline:
         else:
             self.__fanin[to_node] = 0
         self.__dag.add_edge(from_node,to_node,self.__fanin[to_node])
+    def create_pipeline_via_dag(self, dag):
+        self.__dag = dag
+        if self.__id is not None:
+            self.__persisteddag = workflow.get_actor(self.__id)
+            self.__persisteddag.set_dag.run_async(dag)
     def execute_pipeline(self):
         results = self.__dag.execute()
         if self.__id is not None:
-            self.__persisteddag = PersistedDAG.get_or_create(self.__id, self.__dag)
+            self.__persisteddag = workflow.get_actor(self.__id)
+            self.__persisteddag.set_dag.run_async(self.__dag)
         return results
     def return_pipeline(self):
         if self.__id is not None:
-            self.__persisteddag = PersistedDAG.get_or_create(self.__id, self.__dag)
-            return self.__persisteddag
+            self.__persisteddag = workflow.get_actor(self.__id)
+            return self.__persisteddag.get_dag()
         else:
             raise PipelineException('Current pipeline was not saved')
